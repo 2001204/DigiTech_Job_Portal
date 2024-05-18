01a32abaa381740eb062daf45d271750dea8d2ef
Dipak Wani <dipakwani204@gmail.com>
2024-05-19 00:20:34 +0530

    projectupload

---
+++ frontend/src/components/Home/HeroSection.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { FaBuilding, FaSuitcase, FaUsers, FaUserPlus } from "react-icons/fa";
+
+const HeroSection = () => {
+  const details = [
+    {
+      id: 1,
+      title: "1,23,441",
+      subTitle: "Live Job",
+      icon: <FaSuitcase />,
+    },
+    {
+      id: 2,
+      title: "91220",
+      subTitle: "Companies",
+      icon: <FaBuilding />,
+    },
+    {
+      id: 3,
+      title: "2,34,200",
+      subTitle: "Job Seekers",
+      icon: <FaUsers />,
+    },
+    {
+      id: 4,
+      title: "1,03,761",
+      subTitle: "Employers",
+      icon: <FaUserPlus />,
+    },
+  ];
+  
+  return (
+    <>
+      <div className="heroSection">
+        <div className="container">
+          <div className="title">
+           
+            <h2>Discover a Career Path, </h2>
+            <h4>That Matches Your Passions & Abilities</h4>
+            <p>
+            Are you ready to take the next step in your career journey? Look no further! Our job portal is your gateway to exciting opportunities tailored to your interests and skills.
+            </p>
+            <p>Take the first step towards your dream job by browsing our job listings and applying today!</p>
+            <br></br>            <h4><i>Start Exploring Today!</i></h4>
+          </div>
+          <div className="image">
+            <img src="/heroS.jpg" alt="hero" />
+          </div>
+        </div>
+        <div className="details">
+          {details.map((element) => {
+            return (
+              <div className="card" key={element.id}>
+                <div className="icon">{element.icon}</div>
+                <div className="content">
+                  <p>{element.title}</p>
+                  <p>{element.subTitle}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeroSection;
